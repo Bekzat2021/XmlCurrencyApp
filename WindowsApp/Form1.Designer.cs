01cb97ecabba6abb88860eab0e973b50441953d9
Bekzat2021 <bekzat.work@gmail.com>
2020-05-29 22:06:18 +0500

    Adding diapazon between dates and selection+styele

--- WindowsApp/Form1.Designer.cs
+++ WindowsApp/Form1.Designer.cs
@@ -28,18 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.button1 = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(200, 385);
+            this.button1.Location = new System.Drawing.Point(640, 382);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -49,10 +52,10 @@
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
             this.chart1.Location = new System.Drawing.Point(12, 27);
             this.chart1.Name = "chart1";
             this.chart1.Size = new System.Drawing.Size(840, 322);
@@ -75,11 +78,39 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Выберите дату начала";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 364);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Выберите дату окончания";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(206, 385);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(435, 387);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(869, 417);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.chart1);
@@ -98,6 +129,9 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsApp/Form1.cs
+++ WindowsApp/Form1.cs
@@ -18,27 +18,32 @@ namespace WindowsApp
         {
             InitializeComponent();
             dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
             dateTimePicker1.CustomFormat = "yyyy MM dd";
+            dateTimePicker2.CustomFormat = "yyyy MM dd";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DateTime date;
-            date = dateTimePicker1.Value;
-            string s;
-            
+            DateTime date1 = dateTimePicker1.Value;
+            DateTime date2 = dateTimePicker2.Value;
+            int days = (date2 - date1).Days + 1;
+            label3.Text = $"Вы выбрали диапазон в {days.ToString()} дня";
             chart1.Series.Add(currnecy);
             chart1.Series[currnecy].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             chart1.Series[currnecy].IsValueShownAsLabel = true;
+            chart1.Series[currnecy].Color = Color.Lime;
+            chart1.Series[currnecy].BorderWidth = 3;
+            chart1.Series[currnecy].BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.DashDotDot;
             chart1.ChartAreas[0].AxisY.IsStartedFromZero = false;
             chart1.ChartAreas[0].BackColor = Color.LightYellow;
-            for (int i = 0; i < 5; i++)
-            {
-                s = xml.GetData(currnecy, date.AddDays(i));
-                chart1.Series[currnecy].
-                    Points.AddXY(date.AddDays(i).ToString("dd.MM.yyyy"), s);
-            }
 
+            string[] res = xml.GetDataArray(currnecy, date1, days);
+            for (int i = 0; i < days; i++)
+            {
+                chart1.Series[currnecy].
+                    Points.AddXY(date1.AddDays(i).ToString("dd.MM.yyyy"), res[i]);
+            }
         }
     }
 }
