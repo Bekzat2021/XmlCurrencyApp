a86069ba59b127151a397babb85565dccb1db5c0
Bekzat2021 <bekzat.work@gmail.com>
2020-06-04 20:52:20 +0500

    Different colors to currencies and cleraing data

--- WindowsApp/Form1.cs
+++ WindowsApp/Form1.cs
@@ -13,6 +13,7 @@ namespace WindowsApp
     public partial class Form1 : Form
     {
         string currnecy;
+        List<Color> existedColors = new List<Color>();
         NationalBankXmlData xml = new NationalBankXmlData();
         public Form1()
         {
@@ -21,6 +22,23 @@ namespace WindowsApp
             dateTimePicker2.Format = DateTimePickerFormat.Custom;
             dateTimePicker1.CustomFormat = "yyyy MM dd";
             dateTimePicker2.CustomFormat = "yyyy MM dd";
+        }
+
+        public Color GiveRandomColor()
+        {
+            Random random = new Random();
+
+            Color color = new Color();
+            while (true)
+            {
+                color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+
+                if (existedColors.Count < 1)
+                    existedColors.Add(color);
+                else if (!existedColors.Contains(color))
+                    break;
+            }
+            return color;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -45,7 +63,7 @@ namespace WindowsApp
             chart1.Series.Add(currnecy);
             chart1.Series[currnecy].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             chart1.Series[currnecy].IsValueShownAsLabel = true;
-            chart1.Series[currnecy].Color = Color.Lime;
+            chart1.Series[currnecy].Color = GiveRandomColor();
             chart1.Series[currnecy].BorderWidth = 3;
             chart1.Series[currnecy].BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.DashDotDot;
             chart1.ChartAreas[0].AxisY.IsStartedFromZero = false;
@@ -61,6 +79,7 @@ namespace WindowsApp
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
+            chart1.Series.Clear();
             DateTime date1 = dateTimePicker1.Value;
 
             List<string> res2 = xml.GetCurrnecies(date1);
